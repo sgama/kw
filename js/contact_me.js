2424e5e77eb7c2dfbed93516709b75fb00987b06
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-21 03:40:36 +0000

    Deploy sgama/kw to github.com/sgama/kw.git:gh-pages

---
+++ js/contact_me.js
@@ -0,0 +1,1 @@
+$(function(){$("input,textarea").jqBootstrapValidation({preventSubmit:true,submitError:function($form,event,errors){},filter:function(){return $(this).is(":visible");},});$("a[data-toggle=\"tab\"]").click(function(e){e.preventDefault();$(this).tab("show");});});$('#name').focus(function(){$('#success').html('');});
